12459b602c46be3f43f162c8b0b657076f806fa0
yarops <ed.creater@gmail.com>
2026-07-08 12:25:46 +0500

    fix(params): Переносит чувствительную инфу из общих параметров

--- common/config/params.php
+++ common/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'telegram_bot_token' => '8278919268:AAGLHWSjnzQ_MuSMPux6e4YK24591Hxte6I',
-    'telegram_chat_id' => '-1002682077418',
-    'message_thread_id' => '4433',
+    'telegram_bot_token' => null,
+    'telegram_chat_id' => null,
+    'message_thread_id' => null,
 ];
